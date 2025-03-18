9e658e07c9bca44923d95a5d591afcf5870b4b4e
Łukasz <lukkor10@gmail.com>
2025-03-18 22:14:33 +0100

    dodałem plik main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Prosty skrypt obsługujący --help i -h.")
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
